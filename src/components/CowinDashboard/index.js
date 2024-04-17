39a08c34d3b2eecf1c53ad25ea71c40949abfff4
gowthampeddi09 <gowthampeddi09@gmail.com>
2024-04-17 13:21:11 +0000

    Rechats

---
+++ src/components/CowinDashboard/index.js
@@ -0,0 +1,135 @@
+// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import VaccinationCoverage from '../VaccinationCoverage'
+import VaccinationByGender from '../VaccinationByGender'
+import VaccinationByAge from '../VaccinationByAge'
+
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class CowinDashboard extends Component {
+  state = {vaccinationData: {}, apiStatus: apiStatusConstants.initial}
+
+  componentDidMount() {
+    this.getCovidVaccinationData()
+  }
+
+  getCovidVaccinationData = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+
+    const response = await fetch('https://apis.ccbp.in/covid-vaccination-data')
+    if (response.ok) {
+      const covidVaccinationData = await response.json()
+      const updatedData = {
+        last7DaysVaccination: covidVaccinationData.last_7_days_vaccination.map(
+          eachDay => ({
+            vaccineDate: eachDay.vaccine_date,
+            dose1: eachDay.dose_1,
+            dose2: eachDay.dose_2,
+          }),
+        ),
+        vaccinationByAge: covidVaccinationData.vaccination_by_age.map(
+          ageRange => ({
+            age: ageRange.age,
+            count: ageRange.count,
+          }),
+        ),
+        vaccinationByGender: covidVaccinationData.vaccination_by_gender.map(
+          genderType => ({
+            count: genderType.count,
+            gender: genderType.gender,
+          }),
+        ),
+      }
+
+      this.setState({
+        vaccinationData: updatedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  renderVaccinationStats = () => {
+    const {vaccinationData} = this.state
+
+    return (
+      <>
+        <VaccinationCoverage
+          vaccinationCoverageDetails={vaccinationData.last7DaysVaccination}
+        />
+        <VaccinationByGender
+          vaccinationByGenderDetails={vaccinationData.vaccinationByGender}
+        />
+        <VaccinationByAge
+          vaccinationByAgeDetails={vaccinationData.vaccinationByAge}
+        />
+      </>
+    )
+  }
+
+  renderFailureView = () => (
+    <div className="failure-view">
+      <img
+        className="failure-image"
+        src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png"
+        alt="failure view"
+      />
+      <h1 className="failure-text">Something went wrong</h1>
+    </div>
+  )
+
+  renderLoadingView = () => (
+    <div className="loading-view" data-testid="loader">
+      <Loader color="#ffffff" height={80} type="ThreeDots" width={80} />
+    </div>
+  )
+
+  renderViewsBasedOnAPIStatus = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderVaccinationStats()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <div className="cowin-dashboard-container">
+          <div className="logo-container">
+            <img
+              className="logo"
+              src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+              alt="website logo"
+            />
+            <h1 className="logo-heading">Co-WIN</h1>
+          </div>
+          <h1 className="heading">CoWIN Vaccination in India</h1>
+          {this.renderViewsBasedOnAPIStatus()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CowinDashboard
---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,60 @@
+// Write your code here
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const {vaccinationCoverageDetails} = props
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  return (
+    <div className="vaccination-by-coverage-container">
+      <h1 className="vaccination-by-coverage-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={1000}
+        height={300}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 1,
+            fontSize: 16,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 1,
+            fontSize: 16,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+            layout: 'horizontal',
+            align: 'center',
+            fontSize: 12,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" barSize="20%" />
+        <Bar dataKey="dose2" name="Dose 2" fill=" #f54394" barSize="20%" />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
